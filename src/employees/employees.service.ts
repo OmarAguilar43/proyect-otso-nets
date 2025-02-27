0fb8c7133f69c2070ca9285506d60008e942bf08
OmarAguilar43 <omaroaguilar43@gmail.com>
2025-02-27 08:39:51 -0600

    petisions products

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import {v4 as uuid} from 'uuid'
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class EmployeesService {
 
   private  employees:CreateEmployeeDto[] = [{
-    id:1,
+    id:uuid(),
     name:'Alberto',
     lastName:"casares",
     phoneNumber:'7736363'
   },
   {
-    id:2,
+    id:uuid(),
     name:'Jose',
     lastName:"valladares",
     phoneNumber:'32323232'
@@ -20,7 +22,7 @@ export class EmployeesService {
 ]
 
   create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = this.employees.length + 1
+    createEmployeeDto.id = uuid()
     this.employees.push(createEmployeeDto)
     return createEmployeeDto
   }
@@ -31,27 +33,31 @@ export class EmployeesService {
     return this.employees
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
+    
     const employee = this.employees.filter((employee)=>employee.id===id)[0]
+    if(!employee)throw new NotFoundException()
     return employee
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
     let employeeToUpdate = this.findOne(id)
     employeeToUpdate = {
       ...employeeToUpdate,
       ...updateEmployeeDto,
     }
-    this.employees=this.employees.map((employee)=>{
-      if(employee.id === id){
-          employee = employeeToUpdate
-      }
-      return employee
-    })
+    
+    // this.employees=this.employees.map((employee)=>{
+    //   if(employee.id === id){
+    //       employee = employeeToUpdate
+    //   }
+    //   return employee
+    // })
     return employeeToUpdate
   }
 
-  remove(id: number) {
+  remove(id: string) {
+    this.findOne(id)
      this.employees = this.employees.filter((employee)=>employee.id !== id)
      return this.employees
     
---
+++ src/products/products.service.ts
@@ -0,0 +1,78 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import {v4 as uuid} from 'uuid'
+
+@Injectable()
+export class ProductsService {
+  private products:CreateProductDto[]=[
+    {
+      productId:uuid(),
+      productName:'Sabritas normal',
+      price:23,
+      countSeal:5,
+      provider:uuid()
+    },
+    {
+      productId:uuid(),
+      productName:'Koka',
+      price:28,
+      countSeal:3,
+      provider:uuid()
+    },
+    {
+      productId:uuid(),
+      productName:'Aguita gay',
+      price:10,
+      countSeal:9,
+      provider:uuid()
+    }
+  ]
+  create(createProductDto: CreateProductDto) {
+    createProductDto.productId = uuid()
+    createProductDto.provider=uuid()
+    this.products.push(createProductDto)
+    return createProductDto
+  }
+
+  findAll() {
+    return this.products
+  }
+
+  findOne(id: string) {
+    const founded = this.products.filter((founded)=>founded.productId === id)[0]
+    return founded
+    
+  }
+
+  findByProvider(provider: string){
+    const found = this.products.filter((founded)=>founded.provider === provider)
+    if(found.length === 0 ) throw new NotFoundException()
+    return found
+   
+  }
+
+  update(productId: string, updateProductDto: UpdateProductDto) {
+    let productToUpdate = this.findOne(productId)
+    productToUpdate = {
+      ...productToUpdate,
+      ...updateProductDto
+    }
+
+    // this.products=this.products.map((product)=>{
+    //   if(product.productId === productId){
+    //       product = productToUpdate
+    //   }
+    //   return product
+    // })
+    return productToUpdate
+  }
+
+  remove(productId: string) {
+    this.findOne(productId)
+    const founded = this.products.filter((founded)=>founded.productId !== productId)[0]
+    
+    return founded
+    
+  }
+}
